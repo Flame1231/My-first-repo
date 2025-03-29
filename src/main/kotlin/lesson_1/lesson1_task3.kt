7ba8407f4eacf63b2f355a1dc047be708b6081c6
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2025-03-29 18:10:19 +0500

    Add new variable about space

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minutes: Int = 7
+    println(year)
+    println(hour)
+    println(minutes)
+    hour = 10
+    minutes = 55
+    print("$hour:$minutes")
+}
+
