ca3e52c6d02c26478c5022f718c2b7b01f780caf
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2025-03-30 11:46:22 +0500

    Сhanged variable types from Int to String

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_1
 
 fun main() {
-    val year: Int = 1961
-    var hour: Int = 9
-    var minutes: Int = 7
+    val year: String = "1961"
+    var hour: String = "09"
+    var minutes: String = "07"
     println(year)
     println(hour)
     println(minutes)
-    hour = 10
-    minutes = 55
+    hour = "10"
+    minutes = "55"
     print("$hour:$minutes")
 }
 
